be1e67ee8a2deaf88c33144722743a6f40517bb6
Carlos Perez <per.ckarlos@gmail.com>
2023-05-16 20:21:51 -0500

    adding bubble sort and insertion sort

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,45 @@
+﻿
+int[] data = {7,3,8,4,1,9,6,2,5,10};
+
+foreach (int i in data) Console.Write($"{i} ");
+
+/*
+//Buble sort 
+Boolean sorted = false;
+int cnt = data.Length;
+
+
+while (!sorted)
+{
+    sorted = true;
+    for (int i = 0; i < cnt - 1; i++)
+    {
+        if (data[i] > data[i + 1])
+        {
+            int d = data[i];
+            data[i] = data[i + 1];
+            data[i + 1] = d;
+            sorted = false;
+        }
+    }
+    cnt--;
+}*/
+
+// insertion sort
+/*
+for (int i = 1; i < data.Length; i++)
+{
+    int temp = data[i];
+    int j = i-1;
+    while(j>=0 && data[j] > temp)
+    {
+        data[j+1] = data[j];
+        data[j] = temp;
+        j--;
+    }
+}
+*/
+
+
+Console.WriteLine();
+foreach (int i in data) Console.Write(i+" ");
