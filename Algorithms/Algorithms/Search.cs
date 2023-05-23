c487b025444f010e3735324fb4f01e5e21cd8dd7
Carlos Perez <per.ckarlos@gmail.com>
2023-05-23 14:47:14 -0500

    adding and practicing binarySearch

--- Algorithms/Algorithms/Search.cs
+++ Algorithms/Algorithms/Search.cs
@@ -12,24 +12,7 @@ namespace Algorithms
 
         public static Boolean binarySearch(int[] ints, int num, int start, int end)
         {
-            int mid = (start+end)/2;
-            Boolean result = false;
-            if (ints[mid] == num)
-            {
-                result = true;
-            }else if (ints[mid] < num)
-            {
-                binarySearch(ints, num, mid, end);
-            }
-            else if(ints[mid] > num)
-            {
-                binarySearch(ints, num, start, mid);
-            }
-            else
-            {
-                result = false;
-            }
-            return result;
+            
         }
     }
 
