bbfffa323e523925a34bea58d2150efca98d94fb
Carlos Perez <per.ckarlos@gmail.com>
2023-05-30 00:11:05 -0500

    Room and chat added

---
+++ ChatRoom/ChatRoom/Controllers/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms =
+            new Dictionary<int, String>()
+            {
+                { 1, "Cervezas" },
+                { 2, "Programacion" },
+                { 3, "Moda" }
+            };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
