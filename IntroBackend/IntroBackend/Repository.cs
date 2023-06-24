98e1cce6f270f40522712cb37a83c25d64e6012e
Carlos Perez <per.ckarlos@gmail.com>
2023-06-23 21:00:16 -0500

    new repository

---
+++ IntroBackend/IntroBackend/Repository.cs
@@ -0,0 +1,19 @@
+﻿namespace IntroBackend
+{
+    public class Repository
+    {
+        List<Brewery> brewery = new List<Brewery>()
+        {
+            new Brewery() { Id=1, Name = "Minerva"},
+            new Brewery() { Id=2, Name = "Belching Beaver"},
+            new Brewery() { Id=3, Name = "Samichlaus"}
+        };
+    }
+
+    public class Brewery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
