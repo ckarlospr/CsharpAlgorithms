2c708fe282af32c5e368b086c19e85e81b85c7af
Carlos Perez <per.ckarlos@gmail.com>
2023-06-26 23:22:23 -0500

    new end point

--- IntroBackend/IntroBackend/Program.cs
+++ IntroBackend/IntroBackend/Program.cs
@@ -5,6 +5,12 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("brewery", () => new Repository().GetBreweries());
+app.MapGet("/brewery", () => new Repository().GetBreweries());
+app.MapGet("/brewery/{id}", (int id) =>
+{
+    var brewery = new Repository().GetBrewery(id);
+
+    return brewery == null ? Results.NotFound() : Results.Ok(brewery);
+});
 
 app.Run();
