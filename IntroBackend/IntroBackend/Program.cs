6a16853529743348257557e008b811884a6c58c7
Carlos Perez <per.ckarlos@gmail.com>
2023-06-26 22:49:51 -0500

    adding new repository

--- IntroBackend/IntroBackend/Program.cs
+++ IntroBackend/IntroBackend/Program.cs
@@ -1,6 +1,10 @@
+using IntroBackend;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("brewery", () => new Repository().GetBreweries());
 
 app.Run();
--- IntroBackend/IntroBackend/Repository.cs
+++ IntroBackend/IntroBackend/Repository.cs
@@ -2,12 +2,16 @@
 {
     public class Repository
     {
-        List<Brewery> brewery = new List<Brewery>()
+        List<Brewery> breweries = new List<Brewery>()
         {
             new Brewery() { Id=1, Name = "Minerva"},
             new Brewery() { Id=2, Name = "Belching Beaver"},
             new Brewery() { Id=3, Name = "Samichlaus"}
         };
+
+        public List<Brewery> GetBreweries() => breweries;
+        public Brewery? GetBrewery(int id) => breweries.Find(r => r.Id == id);
+
     }
 
     public class Brewery
