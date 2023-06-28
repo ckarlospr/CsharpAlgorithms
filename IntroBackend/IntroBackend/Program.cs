0f4e0f3c8986079e57450e22bcc2ff6335ef1727
Carlos Perez <per.ckarlos@gmail.com>
2023-06-27 22:32:31 -0500

    new chages

--- IntroBackend/IntroBackend/Program.cs
+++ IntroBackend/IntroBackend/Program.cs
@@ -1,4 +1,5 @@
 using IntroBackend;
+using IntroBackend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -11,6 +12,19 @@ app.MapGet("/brewery/{id}", (int id) =>
     var brewery = new Repository().GetBrewery(id);
 
     return brewery == null ? Results.NotFound() : Results.Ok(brewery);
+});
+
+
+app.MapGet("/beers", () => 
+{
+    List<Beer> beers = null;
+
+    using (var db = new PubContext()) 
+    {
+        beers = db.Beers.ToList();
+    }
+
+    return beers;
 });
 
 app.Run();
