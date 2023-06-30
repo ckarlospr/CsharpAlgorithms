2108cd6ed89f14bb8bb68de7f753646b7da9cc60
Carlos Perez <per.ckarlos@gmail.com>
2023-06-29 23:24:15 -0500

    TreeReverse problem solved

---
+++ ExcersicesComplete/ExcersicesTest/Problems.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ExcersicesTest
+{
+    internal class Problems
+    {
+        public void CountLetters(string data)
+        {
+            //string data = "  #$&(Carlos    (&/(%&/$Perez345    Ramirez       ";
+        }
+
+        public void Palindrome(string word)
+        {
+
+        }
+        public Boolean Haming(int[] arr1, int[] arr2)
+        {
+            Boolean result = false;
+
+
+            return result;
+        }
+
+        public void ReverseWord()
+        {
+
+        }
+
+        public void ReverseWords()
+        {
+
+        }
+
+        public void SubString()
+        {
+
+        }
+
+        //Reverse ArrayList
+        //[4]->[3]->[2]->[1]->null
+        //[1]->[2]->[3]->[4]->null
+        /*
+        public Node ArrayListReverse(Node head)
+        {
+
+        }
+        */
+
+        //Reverse Tree
+        /*
+        public void TreeReverse(Tree tree)
+        {
+            Tree temp = tree.Left;
+            tree.left = tree.Right;
+            tree.Right = temp;
+            TreeReverse(tree.Left);
+            TreeReverse(tree.Right);
+        }
+        */
+    }
+}
