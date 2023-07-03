a72fbd2187da4540578e823b5b6b300cc0f800a0
Carlos Perez <per.ckarlos@gmail.com>
2023-07-03 00:45:50 -0500

    bubbleSort completed

--- Alg_AloneFile.cs
+++ Alg_AloneFile.cs
@@ -1,7 +1,19 @@
 //Booble Sort
         public void BubbleSort(int[] arr)
         {
-            
+            Boolean sorted = false;
+            int cnt = arr.Length - 1;
+
+            while(!sorted){
+                 sorted = true;
+                 for(int i=0; i<cnt; i++){
+                    if(arr[i]>arr[i+1]){
+                        sorted=false;
+                        Swap(arr, i, i+1);
+                    }
+                 }
+                 cnt--;
+            }
         }
 
         public void Swap(int[] arr, int pos1, int pos2)
@@ -26,5 +38,20 @@
 
         public int BinarySearch(int[] arr, int n)
         {
-            
+            int start, end, mid, r = -1;
+            start = 0;
+            end = arr.Length;
+
+            while(start<end){
+                mid=(start+end)/2;
+                if(arr[mid]==n){
+                    r = mid;
+                    break;
+                }else if(arr[mid]>n){
+                    start = mid;
+                }else{
+                    end = mid;
+                }
+            }
+            return r;
         }
